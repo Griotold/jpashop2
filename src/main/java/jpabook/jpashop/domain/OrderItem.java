36e912bad89a8003876e2d4431b1f7ad4e9bdb92
Griotold <gotjd9773@naver.com>
2022-12-22 15:52:12 +0900

    주문 도메인 개발 - Order 엔티티 + OrderItem 엔티티 개발

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -27,4 +27,30 @@ public class OrderItem {
     private int orderPrice;
 
     private int count;
+
+    //== 생성 메소드 ==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count){
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+
+    }
+
+    //== 비즈니스 로직==//
+    /**
+     * 주문 취소가 되면 재고 원복
+     * */
+    public void cancel() {
+        
+        getItem().addStock(count);
+    }
+
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+
+    }
 }
