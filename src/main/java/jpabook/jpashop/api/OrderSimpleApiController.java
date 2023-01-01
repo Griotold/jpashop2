53cf7f1de2511e38e215ab92b8d67b0100d4b85c
Griotold <gotjd9773@naver.com>
2023-01-01 17:29:47 +0900

    API 개발 고급 - 간단한 주문 조회 V2 : 엔티티를 DTO로 변환해서 반환

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,13 +1,20 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * xToOne(ManyToOne, OneToOne)
@@ -34,6 +41,45 @@ public class OrderSimpleApiController {
             order.getDelivery().getAddress(); //Lazy 강제 초기환
         }
         return all;
+    }
+    /**
+     * V2.엔티티를 조회해서 DTO로 변환(fetch join 사용X)
+     * * - 단점: 지연로딩으로 쿼리 N번 호출
+     * */
+     
+    @GetMapping("/api/v2/simple-orders")
+    public Result ordersV2() {
+        // ORDER 2개
+        // N + 1 문제 : 1 + 회원(N) + 배송(N) = 총 5번 쿼리
+        
+        List<Order> orders = orderRepository.findAllByCriteria(new OrderSearch());
+        List<SimpleOrderDto> dtos = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+        return new Result(dtos.size(), dtos);
+
+    }
+    @Data // 껍데기 반환값
+    @AllArgsConstructor
+    static class Result<T> {
+        private int count; // count 기능 추가
+        private T data;
+    }
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            this.orderId = order.getId();
+            this.name = order.getMember().getName(); // Lazy 초기화
+            this.orderDate = order.getOrderDate();
+            this.orderStatus = order.getStatus();
+            this.address = order.getDelivery().getAddress(); // Lazy 초기화
+        }
     }
 }
 
